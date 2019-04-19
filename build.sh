678f8ee9092a2fe07f8334b7a2d10cd79d1f1da1
Heriberto Reyes Esparza <hery.chemo@gmail.com>
2019-04-19 15:55:05 -0500

    updated tag version

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@
 # Update parent image
 docker pull jenkins/jenkins:lts
 
-docker build --rm -t herychemo/jenkins:0.1.2 .
+docker build --rm -t herychemo/jenkins:0.1.3 .
 
-docker tag herychemo/jenkins:0.1.2 herychemo/jenkins:latest
+docker tag herychemo/jenkins:0.1.3 herychemo/jenkins:latest
 
 IDS=`docker images | grep "\<none\>" | awk '{print $3}'`
 if [[ ! -z "$IDS" ]]; then
--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 
 docker run -p 8080:8080 \
-    -p 5050:5555 \
     -v //var/run/docker.sock:/var/run/docker.sock  \
     --name localJenkins \
     herychemo/jenkins:latest
